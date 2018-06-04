f5ab755398780ac3654afd0294de6078f26a13d8
97kos <kondmo@gmail.com>
2018-06-04 19:19:11 +0200

    Zaadanie 23.3

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-    const apiRoot = 'http://mighty-ridge-59808.herokuapp.com/v1/';
-    const trelloApiRoot = 'http://mighty-ridge-59808.herokuapp.com/v1/trello/';
+    const apiRoot = 'https://mighty-ridge-59808.herokuapp.com/v1/';
+    const trelloApiRoot = 'https://mighty-ridge-59808.herokuapp.com/v1/trello/';
     const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
     const $tasksContainer = $('[data-tasks-container]');
 
